935ed617d53374aad9eb9481a6345857262b8f5e
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2022-07-13 21:44:30 +0530

    Create 996A_hitTheLottery.cpp

---
+++ codeforces solutions/996A_hitTheLottery.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//                https://codeforces.com/problemset/problem/996/A
+
+
+int main()
+{
+	int n,ans = 0;
+	int a[] = {100,20,10,5,1};
+	cin >> n;
+	for(int i = 0; i < 5; ++i){
+		ans += n/a[i];
+		n = n%a[i];
+	}
+	cout << ans;
+}
