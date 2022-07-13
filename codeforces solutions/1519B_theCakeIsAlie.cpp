2681d7f944a0e32a7b3f3f26e642e7f8f4c0a4d2
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2022-07-13 21:41:24 +0530

    Add files via upload

---
+++ codeforces solutions/1519B_theCakeIsAlie.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+	int n,m,k;
+	cin >> n >> m >> k;
+	vector<vector<int>> dp(n,vector<int> (m,0));
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			if(i == 0 && j == 0) continue;
+			if(i == 0) dp[i][j] = 1+dp[i][j-1];
+			else if(j == 0) dp[i][j] = 1+dp[i-1][j];
+			else dp[i][j] = dp[i-1][j]+(j+1);
+		}
+	}
+	if(dp[n-1][m-1] == k) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+}
