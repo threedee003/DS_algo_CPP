a809fb270bf016070dcca2ad0e7e4a5d547e1fb6
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2022-08-03 17:07:24 +0530

    Create 910A-The Way to Home.cpp

---
+++ codeforces solutions/910A-The Way to Home.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+	int n,d;
+	cin >> n >> d;
+	string s;
+	cin >> s;
+	vector<int> v;
+	for(int i = 0; i < (int)s.size(); ++i){
+		if(s[i] == '1') v.push_back(i);
+	}
+	int curr(0),ans(0);
+	for(int i = 0; i < (int)v.size();){
+		if(v[i]-curr > d){
+			cout << -1;
+			return 0;
+		}
+		while(i < (int)v.size() && v[i]-curr <= d) i++;
+		if(i == (int)v.size()) cout << ans+1;
+		else{
+			curr = v[i-1];
+			ans++;
+		}
+	}
+	return 0;
+}
